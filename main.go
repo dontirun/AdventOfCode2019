1552ab4346d4e2b47a5bba362a6fea13b9d8fb45
Arun Donti <dontirun@gmail.com>
2019-12-04 18:02:35 -0500

    Adding day 4 to main file and remove alias for imports

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 
-	day1 "github.com/dontirun/AdventOfCode2019/day1"
-	day2 "github.com/dontirun/AdventOfCode2019/day2"
-	day3 "github.com/dontirun/AdventOfCode2019/day3"
+	"github.com/dontirun/AdventOfCode2019/day1"
+	"github.com/dontirun/AdventOfCode2019/day2"
+	"github.com/dontirun/AdventOfCode2019/day3"
+	"github.com/dontirun/AdventOfCode2019/day4"
 )
 
-var days = []func(){day1.Solutions, day2.Solutions, day3.Solutions}
+var days = []func(){day1.Solutions, day2.Solutions, day3.Solutions, day4.Solutions}
 
 func main() {
 	for day, solution := range days {
-		fmt.Printf("Details for Day %d: \n", day+1)
+		fmt.Printf("\n Details for Day %d: \n", day+1)
 		solution()
 	}
 }
